06331a061fbd563ae6eae44f20927696f1b3d5e2
Alex Degtyarev <degtyarevaleksandr2005@gmail.com>
2026-01-24 17:33:03 +0300

    feat: Multithreaded operation of the application has been organized

--- cpp-server/src/main.cpp
+++ cpp-server/src/main.cpp
@@ -2,6 +2,7 @@
 #include "database/DataBase.h"
 #include "api/api.h"
 #include <nlohmann/json.hpp>
+#include "mqtt/MQTTClient.h"
 
 using json = nlohmann::json;
 
@@ -9,7 +10,72 @@ int main(){
 
     DataBase db;
     db.open("../Data/smart_home.db");
-    API api(db);
-    api.run();
+
+    // db.deleteDeviceTypesTable();
+    // db.createDeviceTypeTable();
+    // json config;
+    // config["modes"] = {
+    //     "turn_on",
+    //     "turn_off",
+    //     "get_power"
+    // };
+    // std::string s = config.dump();
+    // db.addDeviceType("smart_plug", "actuator", "Умная розетка", s);
+    // config["modes"] = {
+    //     "turn_on",
+    //     "turn_off"
+    // };
+    // db.addDeviceType("relay", "actuator", "Релейный модуль (220В)", config);
+    // config["modes"] = {
+    //     "report_motion",
+    //     "report_no_motion"
+    // };
+    // db.addDeviceType("motion_sensor", "sensor", "Датчик движения (PIR)", config);
+    // config["modes"] = {
+    //     "take_photo",
+    //     "start_recording",
+    //     "end_recording"
+    // };
+    // db.addDeviceType("camera", "actuator", "Камера (RPi)", config);
+    
+    MQTTClient mqtt(db);  // db передается по ссылке
+    
+    // API api(db, mqtt);
+    // api.run();
+
+    // 3. Настройка callback'ов (необязательно)
+    mqtt.setOnConnectCallback([]() {
+        std::cout << "Бизнес-логика: Система готова!" << std::endl;
+    });
+    mqtt.setOnMessageCallback([](const std::string& topic, const std::string& payload) {
+        std::cout << "Бизнес-логика: Получено " << topic << std::endl;
+    });
+    // 4. Подключение (неблокирующее)
+    if (!mqtt.connect("127.0.0.1", 1883)) {
+        // Ошибка инициализации подключения
+        return 1;
+    }
+    
+    mqtt.startLoop();
+
+    API api(db, mqtt);
+    std::thread api_thread([&api]() {
+        std::cout << "Starting HTTP API server..." << std::endl;
+        api.run(); 
+    });
+
+    // 5. Основной цикл приложения
+    while (true) {
+        // 6. Проверка подключения
+        if (mqtt.isConnected()) {
+            
+        }
+        
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+
+    api_thread.join();
+    mqtt.stopLoop();
+
     return 0;
 }
